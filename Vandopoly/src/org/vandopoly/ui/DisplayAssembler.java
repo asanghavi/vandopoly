f739325f6bf5ee3e8d212e3dbe3dce3619de475d
james.d.kasten <james.d.kasten@cfbd7bdc-06e7-11df-a26f-8b716316a5bc>
2010-01-28 21:33:59 +0000

    Added some additional comments and made MainMenu format appropriately

--- Vandopoly/src/org/vandopoly/ui/DisplayAssembler.java
+++ Vandopoly/src/org/vandopoly/ui/DisplayAssembler.java
@@ -1,3 +1,21 @@
+/**
+ *  Vandopoly
+ *  http://code.google.com/p/vandopoly/
+ * 
+ * @author James Kasten
+ * @date Jan 28, 2009
+ * 
+ *  Copyright 2010 Vandopoly Team
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License. 
+ *  You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
+ *  Unless required by applicable law or agreed to in writing, software 
+ *  distributed under the License is distributed on an "AS IS" BASIS, 
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or 
+ *  implied. See the License for the specific language governing 
+ *  permissions and limitations under the License. 
+ */
+
 package org.vandopoly.ui;
 
 import java.awt.Point;
@@ -14,6 +32,7 @@ public class DisplayAssembler {
 		
 	}
 	
+	// Singleton method
 	public static DisplayAssembler getInstance() {
 		if(INSTANCE == null) 
 			INSTANCE = new DisplayAssembler();
@@ -21,13 +40,19 @@ public class DisplayAssembler {
 		return INSTANCE;
 	}
 	
+	// Must be used by Frame to allow access to the DesktopPane
 	public void setDesktopPane(JDesktopPane desktopPane) {
 		desktopPane_ = desktopPane;
 	}
 	
+	// Function allows components to be added to the DesktopPane
 	public void addComponent(JComponent component, Point location, Object layer) {
 		component.setLocation(location);
 		desktopPane_.add(component, layer);
+	}
+	
+	public void removeComponent(JComponent component) {
+		// Needs to be implemented
 	}
 
 }
