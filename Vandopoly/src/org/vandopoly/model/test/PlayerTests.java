5beb22b4ad0b46f6288d8550208c87fef67a7d16
a.mazzia <a.mazzia@cfbd7bdc-06e7-11df-a26f-8b716316a5bc>
2010-02-18 00:40:42 +0000

    Created a test for the functionality of the PlayerState classes

---
+++ Vandopoly/src/org/vandopoly/model/test/PlayerTests.java
@@ -0,0 +1,63 @@
+package org.vandopoly.model.test;
+
+import org.vandopoly.model.Player;
+import org.vandopoly.model.PlayerFree;
+import org.vandopoly.model.PlayerInJail;
+
+public class PlayerTests {
+
+	public static void main(String[] args) {
+		Player A = new Player();
+		Player B = new Player();
+
+		A.goToJail();
+		
+		if (A.getState() == PlayerInJail.Instance()) {
+			System.out.println("Test 1: PASS");
+		}
+		else
+			System.out.print("Test 1: FAIL");
+		
+		if (B.getState() == PlayerFree.Instance()) {
+			System.out.println("Test 2: PASS");
+		}
+		else
+			System.out.println("Test 2: FAIL");
+		
+		A.collectRent(12, B);
+		
+		if (A.getCash() == 0)
+			System.out.println("Test 3: PASS");
+		else
+			System.out.println("Test 3: FAIL");
+		
+		A.updateCash(50);
+		B.collectRent(10, A);
+		
+		if (A.getCash() == 40)
+			System.out.println("Test 4: PASS");
+		else
+			System.out.println("Test 4: FAIL");
+		
+		if (B.getCash() == 10)
+			System.out.println("Test 5: PASS");
+		else
+			System.out.println("Test 5: FAIL");
+		
+		A.getOutOfJail();
+		B.goToJail();
+		
+		if (B.getState() == PlayerInJail.Instance()) {
+			System.out.println("Test 6: PASS");
+		}
+		else
+			System.out.print("Test 6: FAIL");
+		
+		if (A.getState() == PlayerFree.Instance()) {
+			System.out.println("Test 7: PASS");
+		}
+		else
+			System.out.print("Test 7: FAIL");
+	}
+
+}
