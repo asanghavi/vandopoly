6510e8c3d8a888c19e74193a21670577cbf5a2be
a.mazzia <a.mazzia@cfbd7bdc-06e7-11df-a26f-8b716316a5bc>
2010-04-01 00:41:37 +0000

    U5:2 Changed hierarchy of States

---
+++ trunk/Vandopoly/src/org/vandopoly/model/UtilityOwns2.java
@@ -0,0 +1,47 @@
+/*****************************************************************************
+ *   Copyright 2010 Vandopoly Team                                           *
+ *   Licensed under the Apache License, Version 2.0 (the "License");         *
+ *   you may not use this file except in compliance with the License.        *
+ *   You may obtain a copy of the License at                                 *
+ *                                                                           *
+ *   http://www.apache.org/licenses/LICENSE-2.0                              *
+ *                                                                           *
+ *   Unless required by applicable law or agreed to in writing, software     *
+ *   distributed under the License is distributed on an "AS IS" BASIS,       *
+ *   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.*
+ *   See the License for the specific language governing permissions and     *
+ *   limitations under the License.                                          *
+ ****************************************************************************/
+
+package org.vandopoly.model;
+
+/*
+ * UtilityOwns2 class implements the behavior associated with the 
+ * utility property space being owned by the same player who owns the other
+ * utility property space.
+ * ConcreteState class for the State pattern.
+ * 
+ * @author Allie Mazzia
+ */
+public class UtilityOwns2 extends UtilityState{
+
+	private static UtilityOwns2 INSTANCE = null;
+	
+	protected UtilityOwns2() {
+		// Exists to disable instantiation
+	}
+	
+	public static UtilityState Instance() {
+		if (INSTANCE == null) {
+			INSTANCE = new UtilityOwns2();
+		}
+		
+		return INSTANCE;
+	}
+	
+	public void landOn(Player player, UtilitySpace property) {
+		//property.getOwner().collectRent(property.getRentValues()[4], player);
+		// REnt is 10 times amount shown on dice
+	}
+
+}
